67c21e2c412fc2ec855f5626d8021402707b2a2e
dfreemanRIIS <dfreeman@riis.com>
2017-03-20 14:01:41 -0400

    Buttons now write numbers, clear clears

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func zeroPressed(_ sender: UIButton) {
+        let text = (display.text! as String)
+        display.text = text + "0"
+    }
+    
+    @IBAction func onePressed(_ sender: UIButton) {
+        let text = (display.text! as String)
+        display.text = text + "1"
+    }
+    
+    @IBAction func twoPressed(_ sender: UIButton) {
+        let text = (display.text! as String)
+        display.text = text + "2"
+    }
+    
+    @IBAction func threePressed(_ sender: UIButton) {
+        let text = (display.text! as String)
+        display.text = text + "3"
+    }
+    
+    @IBAction func fourPressed(_ sender: UIButton) {
+        let text = (display.text! as String)
+        display.text = text + "4"
+    }
+    
+    @IBAction func fivePressed(_ sender: UIButton) {
+        let text = (display.text! as String)
+        display.text = text + "5"
+    }
+    
+    @IBAction func sixPressed(_ sender: UIButton) {
+        let text = (display.text! as String)
+        display.text = text + "6"
+    }
+    
+    @IBAction func sevenPressed(_ sender: UIButton) {
+        let text = (display.text! as String)
+        display.text = text + "7"
+    }
+    
+    @IBAction func eightPressed(_ sender: UIButton) {
+        let text = (display.text! as String)
+        display.text = text + "8"
+    }
+    
+    @IBAction func ninePressed(_ sender: UIButton) {
+        let text = (display.text! as String)
+        display.text = text + "9"
+    }
 
+    @IBAction func clearPressed(_ sender: UIButton) {
+        display.text = ""
+    }
 }
 
